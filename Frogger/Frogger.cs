fa6a6282f3b9b4e54e9defb089362664ebe56b64
Bas Pijls <b.pijls@hva.nl>
2018-02-15 23:38:07 +0100

    Initial commit

---
+++ Frogger/Frogger.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Frogger
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    class Frogger : GameEnvironment
+    {
+      
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            screen = new Point(520, 300);
+            ApplyResolutionSettings();
+
+            gameStateList.Add(new PlayingState());
+            GameEnvironment.SwitchTo(0);
+
+        }
+
+      
+    }
+}
