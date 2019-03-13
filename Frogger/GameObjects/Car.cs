b1f902eac63e0bde19d8cebdeaa614062a02432e
ikarian7 <scott.marchant7@gmail.com>
2019-03-13 10:15:52 +0100

    frogger maken

---
+++ Frogger/GameObjects/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Frogger
+{
+
+    class Car : GameObject
+    {
+        public Car(String assetName, Vector2 position, Vector2 velocity) : base(assetName)
+        {
+            
+
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            position.X =+ velocity.X;
+
+        }
+    }
+}
--- Frogger/GameStates/PlayingState.cs
+++ Frogger/GameStates/PlayingState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Frogger
 {
@@ -12,6 +13,10 @@ namespace Frogger
         {
             gameObjectList.Add(new GameObject("spr_background"));
             gameObjectList.Add(new Frog());
+            gameObjectList.Add(new Car("spr_truck", new Vector2(70, 225), new Vector2(-2, 0)));
+            gameObjectList.Add(new Car("spr_truck", new Vector2(220, 225), new Vector2(-2, 0)));
+            gameObjectList.Add(new Car("spr_truck", new Vector2(370, 225), new Vector2(-2, 0)));
+
         }
     }
 }
